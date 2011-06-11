50f613a12d65c95be3d974ab0d80a99df06f7ada
Shane Bryzak <sbryzak@gmail.com>
2011-06-12 09:09:28 +1000

    additional category properties, changes to share wizard

--- src/main/java/org/jboss/seam/university/model/Category.java
+++ src/main/java/org/jboss/seam/university/model/Category.java
@@ -16,8 +16,13 @@ import javax.persistence.Id;
 public class Category implements Serializable {
     private static final long serialVersionUID = 8122857831607922307L;
     
+    public enum ContentEditor {richText, url, video, faq, tip, upload};
+    
     private @Id @GeneratedValue Long id;
     private String name;
+    private ContentEditor localEditor;
+    private ContentEditor remoteEditor;
+    private boolean privileged;
     
     public Long getId() {
         return id;
@@ -33,5 +38,29 @@ public class Category implements Serializable {
     
     public void setName(String name) {
         this.name = name;
+    }
+       
+    public ContentEditor getLocalEditor() {
+        return localEditor;
+    }
+    
+    public void setLocalEditor(ContentEditor localEditor) {
+        this.localEditor = localEditor;
+    }
+    
+    public ContentEditor getRemoteEditor() {
+        return remoteEditor;
+    }
+    
+    public void setRemoteEditor(ContentEditor remoteEditor) {
+        this.remoteEditor = remoteEditor;
+    }
+    
+    public boolean isPrivileged() {
+        return privileged;
+    }
+    
+    public void setPrivileged(boolean privileged) {
+        this.privileged = privileged;
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -19,14 +19,14 @@ insert into IdentityObjectRelationshipType (id, name) values (2, 'JBOSS_IDENTITY
 
 insert into IdentityObjectRelationship (id, name, relationship_type_id, from_identity_id, to_identity_id) values (1, 'admin', 2, 3, 2);
 
-insert into Category (id, name) values (1, 'Article');
-insert into Category (id, name) values (2, 'Blog');
-insert into Category (id, name) values (3, 'Tip');
-insert into Category (id, name) values (4, 'FAQ');
-insert into Category (id, name) values (5, 'Reference Documentation');
-insert into Category (id, name) values (6, 'API Documentation');
-insert into Category (id, name) values (7, 'News');
-insert into Category (id, name) values (8, 'IRC Log');
-insert into Category (id, name) values (9, 'Video');
-insert into Category (id, name) values (10, 'Podcast');
-insert into Category (id, name) values (11, 'Presentation');
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (1, 'Article', 0, 1, false);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (2, 'Blog', 0, 1, false);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (3, 'Tip', 4, null, false);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (4, 'FAQ', 3, null, false);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (5, 'Reference Documentation', null, 1, true);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (6, 'API Documentation', null, 1, true);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (7, 'News', 0, 1, false);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (8, 'IRC Log', 5, 1, true);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (9, 'Video', 2, 1, false);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (10, 'Podcast', 5, 1, false);
+insert into Category (id, name, localEditor, remoteEditor, privileged) values (11, 'Presentation', 5, 1, false);
