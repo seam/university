0545c32769ada59ca3fcb5eca8b6700037d18e3d
Shane Bryzak <sbryzak@gmail.com>
2011-06-19 19:51:54 +1000

    implement content save

--- src/main/java/org/jboss/seam/university/action/ContentAction.java
+++ src/main/java/org/jboss/seam/university/action/ContentAction.java
@@ -1,12 +1,16 @@
 package org.jboss.seam.university.action;
 
+import java.util.Date;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.jcr.Node;
 import javax.jcr.Repository;
 import javax.jcr.Session;
 
 import org.jboss.seam.jcr.annotations.JcrConfiguration;
 import org.jboss.seam.remoting.annotations.WebRemote;
+import org.jboss.seam.university.model.Category;
 
 /**
  * 
@@ -15,19 +19,40 @@ import org.jboss.seam.remoting.annotations.WebRemote;
  */
 public @RequestScoped class ContentAction {
 
-    @Inject @JcrConfiguration(name="org.modeshape.jcr.URL", value="modeshape-config.xml?repositoryName=repository")
+    //@Inject Logging log;
+    
+    @Inject @JcrConfiguration(name="org.modeshape.jcr.URL", value="jndi:jcr/local?repositoryName=repository")
     Repository repository;
     
-    @Inject @JcrConfiguration(name="org.modeshape.jcr.URL",value="modeshape-config.xml?repositoryName=repository")
-    Session session;
+    //@Inject @JcrConfiguration(name="org.modeshape.jcr.URL",value="jndi:jcr/local?repositoryName=repository")
+    //Session session;
     
     @WebRemote
-    public boolean save() {
-
-        String user = session.getUserID();
-        String name = repository.getDescriptor(Repository.REP_NAME_DESC);
-        System.out.println("Logged in as " + user + " to a " + name + " repository."); 
+    public boolean saveLocalContent(Category category, String title, String content) throws Exception  {   
         
-        return true;
+        if (title == null) {
+            throw new IllegalArgumentException("Error - title parameter cannot be null");
+        }
+        
+        Session session = repository.login("default");
+        try {
+            
+            String user = session.getUserID();
+            String name = repository.getDescriptor(Repository.REP_NAME_DESC);           
+            
+            Node rootNode = session.getRootNode();            
+            Node categoryParent = rootNode.hasNode(category.getName()) ? rootNode.getNode(category.getName()) :
+                rootNode.addNode(category.getName());
+
+            Node contentNode = categoryParent.addNode(title);
+            contentNode.setProperty("content", content);
+            contentNode.setProperty("created", System.currentTimeMillis());
+            session.save();
+            
+            return true;
+        }
+        finally {
+            session.logout();
+        }
     }
 }
